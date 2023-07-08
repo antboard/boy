3e0e6a531745d53c8e9c14ec553a57276a506d39
jiangyichun <icecut@qq.com>
2023-07-08 11:32:00 +0800

    + 添加注释

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -12,15 +13,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "boy",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "一个 golang语言的工程管理工具",
+	Long:  `可以很容易的创建一个工程, 集成一些常用的包, 欢迎提出建设意见`,
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
@@ -34,13 +28,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.boy.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	fmt.Println("    __\n   / /_  ____  __  __\n  / __ \\/ __ \\/ / / /\n / /_/ / /_/ / /_/ /\n/_.___/\\____/\\__, /\n            /____/")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
